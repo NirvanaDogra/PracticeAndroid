a56dfd6c30686a0a1ee7511d1fd12efb03877785
Nirvana Dogra <nirvanadogra004@gmail.com>
2024-09-30 12:14:30 -0500

    added second screen content

--- intents/app/src/main/java/com/nirvana/myapplication/SecondActivity.kt
+++ intents/app/src/main/java/com/nirvana/myapplication/SecondActivity.kt
@@ -7,12 +7,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nirvana.myapplication.ui.theme.MyApplicationTheme
 
 
@@ -41,5 +50,15 @@ fun SecondActivityScreen(innerPadding: PaddingValues) {
         }
         context?.setResult(Activity.RESULT_OK, resultIntent)
         context?.finish()
+    }
+
+    Box(
+        modifier = Modifier
+        .fillMaxSize()
+        .border(BorderStroke(4.dp, Color.Green))
+        .background(Color.Red)
+        .padding(4.dp),
+    ) {
+
     }
 }
